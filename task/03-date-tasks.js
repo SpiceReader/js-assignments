b131891a9ecd9541d9770d104755179a250f0153
Artem Yaulkovski <54952838+SpiceReader@users.noreply.github.com>
2019-09-15 19:31:34 +0300

    added 3 func

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,23 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+  let d = date.getFullYear();
+  if (d%4 != 0)
+  {
+    return false;
+  }
+  else if (d%100 != 0)
+  {
+    return true;
+  }
+  else if (d%400 != 0)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
 }
 
 
@@ -94,7 +110,8 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    DateinHours = date.getUTCHours();
+    return DateinHours*Math.PI/24;
 }
 
 
