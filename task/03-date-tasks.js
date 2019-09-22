c7cf619091dfd85414a121fbcc2df41bd5ad57f3
Artem Yaulkovski <54952838+SpiceReader@users.noreply.github.com>
2019-09-22 18:06:50 +0300

    added timeSpanToString func

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -92,7 +92,9 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    const TimeSpan = new Date(endDate - startDate);
+    const TimeSpantoStr = TimeSpan.toISOString();  
+    return TimeSpantoStr.slice(11, -1); 
 }
 
 
